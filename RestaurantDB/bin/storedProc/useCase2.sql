363c0c18cfc35024041bedf932ed064153ca038f
erikjearl <erikjearl@gmail.com>
2022-12-01 19:00:32 -0500

    use case 2 sql

--- RestaurantDB/bin/storedProc/useCase2.sql
+++ RestaurantDB/bin/storedProc/useCase2.sql
@@ -4,22 +4,18 @@ create procedure MakeNewMenuItem
 @menu_item_name  varchar(20),
 @price  numeric(10,2),
 @ingredent varchar(20),
-@equipment varchar(20),
-
-@id int output
+@equipment varchar(20)
 
 as
 
 begin
 
-insert into menuItem (name, price)  values(@price, @menu_item_name);
+insert into MenuItem (menuItemName, price) values (@price, @menu_item_name);
 
-insert into recipie (IngredientId, MenuItemId, EquipmentId) values(
-    SELECT id from ingredent where ingredentId = @ingredent,
-    SELECT max(id) from MenuItem,
-    SELECT id from equipment where EquipmentId = @equipment,
-)
-
-select @id = SCOPE_IDENTITY();
+insert into Recipe (ingredientID, menuItemID, equipmentID) values(
+    (SELECT ID from Ingredent where ingredientID = @ingredent),
+    (SELECT max(ID) from MenuItem),
+    (SELECT ID from Equipment where equipmentID = @equipment),
+);
 
 end
