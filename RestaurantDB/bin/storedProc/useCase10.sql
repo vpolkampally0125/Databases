8d78427a43af33e8ae8e35774389c5872df35732
erikjearl <erikjearl@gmail.com>
2022-12-02 00:30:41 -0500

    9 need fix

---
+++ RestaurantDB/bin/storedProc/useCase10.sql
@@ -0,0 +1,16 @@
+
+create procedure FindSupplierIngredients
+
+    @supplier_name varchar(20),
+    @out varchar(256) OUTPUT
+
+as
+
+begin
+
+    Select distinct ingredientName from Ingredient 
+        Join Supplies on Ingredient.ID = Supplies.supplierID
+        Join Supplier on Supplies.supplierID = Supplier.ID
+    Where supplierName = @supplier_name
+
+end
