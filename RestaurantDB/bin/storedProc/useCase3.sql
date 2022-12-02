5eae014cf543562a4150c279b91f8fcc689ff396
erikjearl <erikjearl@gmail.com>
2022-12-01 20:02:42 -0500

    USE CASE 3 WORKS

--- RestaurantDB/bin/storedProc/useCase3.sql
+++ RestaurantDB/bin/storedProc/useCase3.sql
@@ -1,19 +1,34 @@
 
 create procedure ChangeMenuItem
 
-@menu_item_name varchar(20)
-@ingredient varchar
-(20)
-@equipment varchar
-(20)
+    @menu_item_name varchar(20),
+    @ingredient_replaced varchar(20),
+    @ingredient_replacing varchar(20),
+    @equipment_replaced varchar(20),
+    @equipment_replacing varchar(20)
 
 as
 
-update Recipe
-set Ingredient
-
 begin
 
+    update Recipe
+set ingredientID = (SELECT ID
+    from Ingredient
+    where ingredientName =@ingredient_replacing)
+where (SELECT ID
+        from MenuItem
+        where @menu_item_name = menuItemName) = Recipe.menuItemID and Recipe.ingredientID = (SELECT ID
+        from Ingredient
+        where ingredientName =@ingredient_replaced)
 
+    update Recipe
+set equipmentID = (SELECT ID
+    from Equipment
+    where equipmentName =@equipment_replacing)
+where (SELECT ID
+        from MenuItem
+        where @menu_item_name = menuItemName) = Recipe.menuItemID and Recipe.equipmentID = (SELECT ID
+        from Equipment
+        where equipmentName =@equipment_replaced)
 
-    end
+end
