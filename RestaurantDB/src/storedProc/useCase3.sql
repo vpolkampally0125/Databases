d43da6d31e92e8754d81106c86da81a6f4b1bc26
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2022-12-01 19:45:54 -0500

    finished use case 3

--- RestaurantDB/src/storedProc/useCase3.sql
+++ RestaurantDB/src/storedProc/useCase3.sql
@@ -1,19 +1,35 @@
 
 create procedure ChangeMenuItem
 
-@menu_item_name varchar(20)
-@ingredient varchar
-(20)
-@equipment varchar
-(20)
+    @menu_item_name varchar(20),
+    @ingredient_replaced varchar(20),
+    @ingredient_replacing varchar(20),
+    @equipment_replaced varchar(20),
+    @equipment_replacing varchar(20)
 
 as
 
-update Recipe
-set Ingredient
-
 begin
 
+    update Recipe
+set ingredientID = (SELECT ID
+    from Ingredient
+    where name =@ingredient_replacing)
+where (SELECT ID
+        from MenuItem
+        where @menu_item_name = name) = Recipe.MenuItemID and Recipe.Ingredient.ID = (SELECT ID
+        from Ingredient
+        where name =@ingredient_replaced)
+
+    update Recipe
+set equipmentID = (SELECT ID
+    from Equipment
+    where name =@equipment_replacing)
+where (SELECT ID
+        from MenuItem
+        where @menu_item_name = name) = Recipe.MenuItemID and Recipe.Equipment.ID = (SELECT ID
+        from Equipment
+        where name =@equipment_replaced)
 
 
-    end
+end
