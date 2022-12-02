ec927880182146350b574d56afab87e1700db1b4
erikjearl <erikjearl@gmail.com>
2022-12-01 20:16:18 -0500

    USE CASE 5 WORKS

--- RestaurantDB/src/DDL/setPermissions.sql
+++ RestaurantDB/src/DDL/setPermissions.sql
@@ -4,4 +4,5 @@ USE restaurantDB;
 	GRANT EXECUTE ON OBJECT::MakeNewMenuItem TO dbuser;
 	GRANT EXECUTE ON OBJECT::ChangeOrder TO dbuser;
 	GRANT EXECUTE ON OBJECT::ChangeMenuItem TO dbuser;
+	GRANT EXECUTE ON OBJECT::DeleteMenuItem TO dbuser;
 GO
