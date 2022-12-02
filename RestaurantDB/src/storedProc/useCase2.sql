f82da1e3c72a2ba34a413cd501576034992e924b
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2022-12-01 19:06:07 -0500

    Update useCase2.sql

--- RestaurantDB/src/storedProc/useCase2.sql
+++ RestaurantDB/src/storedProc/useCase2.sql
@@ -1,21 +1,31 @@
 
 create procedure MakeNewMenuItem
 
-@menu_item_name  varchar(20),
-@price  numeric(10,2),
-@ingredent varchar(20),
-@equipment varchar(20)
+    @menu_item_name  varchar(20),
+    @price  numeric(10,2),
+    @ingredent varchar(20),
+    @equipment varchar(20)
 
 as
 
 begin
 
-insert into MenuItem (menuItemName, price) values (@price, @menu_item_name);
+    insert into MenuItem
+        (menuItemName, price)
+    values
+        (@price, @menu_item_name);
 
-insert into Recipe (ingredientID, menuItemID, equipmentID) values(
-    (SELECT ID from Ingredent where ingredientID = @ingredent),
-    (SELECT max(ID) from MenuItem),
-    (SELECT ID from Equipment where equipmentID = @equipment),
+    insert into Recipe
+        (ingredientID, menuItemID, equipmentID)
+    values(
+            (SELECT ID
+            from Ingredent
+            where ingredientID = @ingredent),
+            (SELECT max(ID)
+            from MenuItem),
+            (SELECT ID
+            from Equipment
+            where equipmentID = @equipment)
 );
 
 end
