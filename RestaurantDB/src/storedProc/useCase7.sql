3f8909d9ab5de54da979e50b84b42ae42e673c99
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2022-12-01 20:52:17 -0500

    use case

--- RestaurantDB/src/storedProc/useCase7.sql
+++ RestaurantDB/src/storedProc/useCase7.sql
@@ -15,8 +15,8 @@ begin
     (SELECT ID
         from MenuItem
         where @menu_item = menuItemName)
-)) as item_recipe
-    where item_recipe.ingredientID = recipeID
+)) as item_recipe, Ingredient
+    where item_recipe.ingredientID = ingredientID
 
 
 end
--- RestaurantDB/src/storedProc/useCase9.sql
+++ RestaurantDB/src/storedProc/useCase9.sql
@@ -7,6 +7,21 @@ as
 
 begin
 
+    
+    
+    SELECT *
+    from (SELECT *
+    from (SELECT *
+    from Customer, Buys 
+    where Customer.id = Buys.Customer.id and @customer_name=customerName) as A, Orders 
+    where A.orderID = Orders.ID) as B, OrderItems 
+    where B.Order.id = OrderItems.id
+    ) as C
+    SELECT *
+    from C, MenuItems 
+    where C.OrderItems.id = OrderItems.id
+
+
     (((SELECT *
     from Customer, Buys 
     where Customer.id = Buys.Customer.id and @customer_name=customerName)
